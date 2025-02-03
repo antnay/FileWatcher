3e1bfa66b35b98c74fa603afee1d9b1cb9881867
Anthony <antnay@uw.edu>
2025-02-03 00:45:13 -0800

    Feature: Expand database model class.

--- src/controller/FileWatcherController.java
+++ src/controller/FileWatcherController.java
@@ -2,17 +2,11 @@ package controller;
 
 import model.DBManager;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class FileWatcherController {
 
     public static void main(String[] theArgs) {
-        try (Connection connect = DBManager.connect()) {
-        } catch (SQLException theE) {
-            // FIXME
-            System.err.println(theE.getMessage());
-            System.exit(1);
-        }
+        DBManager.getDBManager().connect();
+        DBManager.getDBManager().addToTable("test 1");
     }
 }
--- src/model/DBManager.java
+++ src/model/DBManager.java
@@ -6,10 +6,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class DBManager {
-    public static Connection connect() {
+public final class DBManager {
+
+    private static final DBManager DB_INSTANCE = new DBManager();
+
+    private Connection connection;
+
+    private DBManager() {
+        connection = null;
+        // TODO: Maybe call connect() right away?
+    }
+
+    public static DBManager getDBManager() {
+        return DB_INSTANCE;
+    }
+
+    public void connect() {
         File dbPath = new File("database/log.db");
         if (!Files.exists(Path.of(dbPath.getAbsolutePath()))) {
             try {
@@ -20,15 +37,74 @@ public class DBManager {
                 System.exit(1);
             }
         }
-
         String dbUrl = "jdbc:sqlite:" + dbPath.getAbsolutePath();
-        Connection dbCon = null;
         try {
-             dbCon = DriverManager.getConnection(dbUrl);
+            connection = DriverManager.getConnection(dbUrl);
+            initDB();
         } catch (SQLException theE) {
             System.err.println("Error connecting to database " + theE.getMessage());
-            System.exit(1);
+            // TODO: Handle this error
         }
-        return dbCon;
     }
+
+    public void disconnect() {
+        try {
+            if (!isConnected()) {
+                connection.close();
+                connection = null;
+            }
+        } catch (SQLException theE) {
+            System.err.println("Error trying to close database.");
+            // TODO: Handle this error
+        }
+    }
+
+    // test
+    public void addToTable(String event) {
+        String insertSQL = "INSERT INTO eventlog(event) VALUES(?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setString(1, event);
+            preparedStatement.executeUpdate();
+            System.out.println("Inserted event: " + event);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error inserting event", e);
+            // TODO: Handle this error
+        }
+    }
+
+    private void initDB() {
+        if (!isConnected()) {
+            throw new IllegalStateException("Cannot add to table while disconnected");
+        }
+        try (Statement statement = connection.createStatement()) {
+            ResultSet res = statement.executeQuery(
+                    "SELECT name FROM sqlite_master WHERE type='table' AND name='eventlog';"
+            );
+            if (!res.next()) {
+                System.out.println("making new table");
+                statement.executeUpdate("CREATE TABLE \"eventlog\" (\n" +
+                        "\t\"id\"\tINTEGER NOT NULL UNIQUE,\n" +
+                        "\t\"event\"\tTEXT,\n" +
+                        "\t\"path\"\tTEXT,\n" +
+                        "\t\"Timestamp\"\tDATETIME DEFAULT CURRENT_TIMESTAMP,\n" +
+                        "\tPRIMARY KEY(\"id\" AUTOINCREMENT)\n" +
+                        ");");
+                System.out.println("added table");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+            // TODO: Handle this error
+        }
+
+    }
+
+    /**
+     * Checks connection to database.
+     *
+     * @return true if there is a connection to the database, false otherwise.
+     */
+    private boolean isConnected() {
+        return connection != null;
+    }
+
 }
