b048f25e99fb0c5aa46e5f06ae768dcd1cb4af35
Anthony <antnay@uw.edu>
2025-02-07 21:18:33 -0800

    feat: temporary testing of system watch from controller

--- src/controller/FileWatcherController.java
+++ src/controller/FileWatcherController.java
@@ -1,13 +1,17 @@
 package controller;
 
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import java.nio.file.Path;
+
 import model.DBManager;
+import model.SystemWatch;
 
 public class FileWatcherController {
 
-    private FileWatcherController() {
+    private SystemWatch sysWatch;
+
+    public FileWatcherController() {
+        sysWatch = new SystemWatch();
+        sysWatch.addDir(Path.of(System.getProperty("user.home")));
+        sysWatch.startWatch();
     }
-
-
 }
