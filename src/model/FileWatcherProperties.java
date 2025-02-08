96993e89d3c03e63085aba897fce506f393661da
Anthony <antnay@uw.edu>
2025-02-07 23:07:58 -0800

    feat: properties interface

---
+++ src/model/FileWatcherProperties.java
@@ -0,0 +1,7 @@
+package model;
+
+public interface FileWatcherProperties {
+    public static final String START = "start";
+    public static final String STOP = "stop";
+    public static final String SUBMIT = "submit";
+}
