adadde89fabf2dfbba2a330e68f4db29bc7c6501
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2025-02-18 16:14:20 -0800

    Implemented GridBagLayout for control components

--- src/view/ControlPanel.java
+++ src/view/ControlPanel.java
@@ -1,9 +1,12 @@
 package view;
 
+import java.awt.*;
 import java.beans.PropertyChangeSupport;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 // set file extensions and directories
 class ControlPanel extends JPanel {
@@ -13,9 +16,16 @@ class ControlPanel extends JPanel {
     private JButton mySubmitButton;
     private JButton myClearButton; // temp
     private JButton mySaveLogButton;
+    private JLabel myExtensionLabel;
+    private JTextField myFileExtension;
+    private JLabel myDirectoryLabel;
+    private JTextField myDirectory;
+    private final GridBagConstraints gridConstraints = new GridBagConstraints();
 
     ControlPanel(PropertyChangeSupport thePcs) {
         myPCS = thePcs;
+        setLayout(new GridBagLayout());
+        initTextFields();
         initButtons();
     }
 
@@ -23,29 +33,43 @@ class ControlPanel extends JPanel {
         myStartStopButton.setText((theIsRunning) ? "Stop" : "Start");
     }
 
+    private void initTextFields() {
+        JLabel instructionLabel = new JLabel("Select a file extension, a directory, and click Start to begin File Watcher.");
+
+        add(instructionLabel);
+    }
+
     private void initButtons() {
         myStartStopButton = new JButton("Start");
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 1;
         myStartStopButton.addActionListener(theEvent -> {
             myPCS.firePropertyChange(ViewProperties.START_STOP_BUTTON, null, null);
         });
-        add(myStartStopButton);
+        add(myStartStopButton, gridConstraints);
 
         mySubmitButton = new JButton("Submit");
+        gridConstraints.gridx = 2;
+        gridConstraints.gridy = 1;
         mySubmitButton.addActionListener(theEvent -> {
             myPCS.firePropertyChange(ViewProperties.SUBMIT_BUTTON, null, null);
         });
-        add(mySubmitButton);
+        add(mySubmitButton, gridConstraints);
 
         myClearButton = new JButton("Clear");
+        gridConstraints.gridx = 3;
+        gridConstraints.gridy = 1;
         myClearButton.addActionListener(theEvent -> {
             myPCS.firePropertyChange(ViewProperties.CLEAR_LOG_BUTTON, null, null);
         });
-        add(myClearButton);
+        add(myClearButton, gridConstraints);
 
         mySaveLogButton = new JButton("Save");
+        gridConstraints.gridx = 4;
+        gridConstraints.gridy = 1;
         mySaveLogButton.addActionListener(theEvent -> {
             myPCS.firePropertyChange(ViewProperties.SAVE_LOG, null, null);
         });
-        add(mySaveLogButton);
+        add(mySaveLogButton, gridConstraints);
     }
 }
