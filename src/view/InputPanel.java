233311b82296b8d40a54c4f1b5f46d8c54bea255
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2025-02-22 18:01:01 -0800

    Implemented BorderLayout and created an InputPanel for user input

---
+++ src/view/InputPanel.java
@@ -0,0 +1,65 @@
+package view;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.*;
+import java.beans.PropertyChangeSupport;
+
+public class InputPanel extends JPanel {
+    private PropertyChangeSupport myPCS;
+
+    public InputPanel(PropertyChangeSupport thePcs) {
+        myPCS = thePcs;
+        initInputFields();
+    }
+
+    private void initInputFields() {
+        BorderLayout layout = new BorderLayout();
+        layout.setVgap(10);
+        layout.setHgap(10);
+        setLayout(layout);
+
+        // label telling the user what to do
+        JLabel instructionLabel = new JLabel("Select a file extension, a directory, and click Start to begin File Watcher.");
+        instructionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(instructionLabel, BorderLayout.NORTH);
+
+        // add the input fields for file extension and file directory
+        add(createExtensionPanel(), BorderLayout.WEST);
+        add(createDirectoryPanel(), BorderLayout.EAST);
+
+        // start and stop buttons
+    }
+
+    private JPanel createExtensionPanel() {
+        // label for extension field
+        JLabel extensionLabel = new JLabel("Monitor by extension");
+
+        // text field for extension
+        JTextField extensionField = new JTextField();
+
+        JPanel extensionPanel = new JPanel(new BorderLayout());
+        extensionPanel.add(extensionLabel, BorderLayout.NORTH);
+        extensionPanel.add(extensionField, BorderLayout.CENTER);
+
+        return extensionPanel;
+    }
+
+    private JPanel createDirectoryPanel() {
+        // label for directory field
+        JLabel directoryLabel = new JLabel("Directory to monitor");
+
+        // text field for directory
+        JTextField directoryField = new JTextField();
+        directoryField.setColumns(40);
+
+        JPanel directoryPanel = new JPanel(new BorderLayout());
+        directoryPanel.add(directoryLabel, BorderLayout.NORTH);
+        directoryPanel.add(directoryField, BorderLayout.CENTER);
+
+
+        return directoryPanel;
+    }
+}
