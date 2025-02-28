fee42265d3c85144c09d1c7b315a99d3c7fa3908
Anthony <antnay@uw.edu>
2025-02-27 16:31:54 -0800

    chore: fix interface

--- src/view/ViewProperties.java
+++ src/view/ViewProperties.java
@@ -1,12 +1,12 @@
 package view;
 
 public interface ViewProperties {
-    public static final String START_BUTTON = "start";
-    public static final String STOP_BUTTON = "stop";
-    public static final String SUBMIT_BUTTON = "submit";
-    public static final String CLEAR_LOG_BUTTON = "clearLog";
-    public static final String SAVE_LOG = "save";
-    public static final String SHORTCUTS = "shortcut";
-    public static final String ABOUT = "about";
-    public static final String START_STOP_BUTTON = "startStop";
+    String START_BUTTON = "startView";
+    String STOP_BUTTON = "stopView";
+    String SUBMIT_BUTTON = "submitView";
+    String CLEAR_LOG_BUTTON = "clearLogView";
+    String SAVE_LOG = "saveView";
+    String SHORTCUTS = "shortcutView";
+    String ABOUT = "aboutView";
+    String START_STOP_BUTTON = "startStopView";
 }
