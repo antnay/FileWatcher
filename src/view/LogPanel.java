7d01c78bbf3d3b47b3b0ad4c07e5477dfc74e5c8
Mitchell Nowasky <82383787+MitchellCodes@users.noreply.github.com>
2025-03-08 03:05:37 -0800

    Added a check to only add to the log table if the extension and directory combination is in the watched files table

--- src/view/LogPanel.java
+++ src/view/LogPanel.java
@@ -8,18 +8,21 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import controller.FileListController;
 import model.Event;
 
 // log under config view
 class LogPanel extends JPanel {
 
     // private PropertyChangeSupport myPCS;
+    private final JTable myFileListTableReference;
     private DefaultTableModel myTableModel;
     private JTable myLogTable;
     private JScrollPane myLogContainer;
 
     LogPanel(PropertyChangeSupport thePcs) {
         // myPCS = thePcs;
+        myFileListTableReference = FileListController.getFileListTable();
         myTableModel = new DefaultTableModel();
         myLogTable = new JTable(myTableModel);
         myLogContainer = new JScrollPane(myLogTable);
@@ -30,7 +33,21 @@ class LogPanel extends JPanel {
     }
 
     void addEvent(Event theEvent) {
-        myTableModel.addRow(theEvent.toArray());
+        if (isCombinationInTable(theEvent.getMyExtension(), theEvent.getPath(), myFileListTableReference)) {
+            myTableModel.addRow(theEvent.toArray());
+        }
+    }
+
+    public boolean isCombinationInTable(final String theExtension, final String theDirectory, final JTable theTable) {
+        boolean comboIsInTable = false;
+        for (int i = 0; i < theTable.getRowCount(); i++) {
+            String currentExtension = theTable.getValueAt(i, 0).toString();
+            String currentDirectory = theTable.getValueAt(i, 1).toString();
+            if (currentExtension.equals(theExtension) && currentDirectory.equals(theDirectory)) {
+                comboIsInTable = true;
+            }
+        }
+        return comboIsInTable;
     }
 
     private void initTable() {
