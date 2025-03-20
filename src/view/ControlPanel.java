214512fe68503aa546e5956e9c84632a247aac48
YosanT <yosan004@gmail.com>
2025-03-19 20:56:07 -0700

    JavaDoc for ControlPanel

--- src/view/ControlPanel.java
+++ src/view/ControlPanel.java
@@ -6,6 +6,10 @@ import java.beans.PropertyChangeSupport;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+/**
+ * The ControlPanel class creates the main panel for user input.
+ * It arranges the layout and links to other UI components.
+ */
 // set file extensions and directories
 class ControlPanel extends JPanel {
 
@@ -15,6 +19,11 @@ class ControlPanel extends JPanel {
 //    private JButton myClearButton; // temp
 //    private JButton mySaveLogButton;
 
+    /**
+     * Creates the control panel and sets up the input section.
+     *
+     * @param thePcs Handles communication between components when changes occur.
+     */
     ControlPanel(PropertyChangeSupport thePcs) {
         myPCS = thePcs;
         setLayout(new BorderLayout(10, 10));
