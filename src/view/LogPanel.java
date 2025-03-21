8e34a0125c465399542ed030019454f179fc41bc
Mitchell Nowasky <82383787+MitchellCodes@users.noreply.github.com>
2025-03-21 11:49:31 -0700

    Added javadoc to LogPanel

--- src/view/LogPanel.java
+++ src/view/LogPanel.java
@@ -13,21 +13,39 @@ import javax.swing.table.TableModel;
 
 import model.ModelProperties;
 
-// log under config view
+/**
+ * An element of the GUI that shows the user a log of changes that were caught by the watcher system.
+ */
 class LogPanel extends JPanel implements PropertyChangeListener {
-    private final PropertyChangeSupport myPCS;
+    /**
+     * The JTable displayed in this panel.
+     */
     private final JTable myJTable;
 
+    /**
+     * Constructs a panel that displays a log of changes caught by the watcher system in a table.
+     * When notified of the table model being changed, the displayed table will update.
+     *
+     * @param thePcs The <code>PropertyChangeSupport</code> that this listener should be added to.
+     */
     public LogPanel(PropertyChangeSupport thePcs) {
-        myPCS = thePcs;
-        myPCS.addPropertyChangeListener(this);
+        thePcs.addPropertyChangeListener(this);
+
         myJTable = new JTable(new DefaultTableModel());
-        JScrollPane tableContainer = new JScrollPane(myJTable);
         myJTable.setEnabled(false);
+        JScrollPane tableContainer = new JScrollPane(myJTable);
+
         setLayout(new BorderLayout());
         add(tableContainer, BorderLayout.CENTER);
     }
 
+    /**
+     * This method gets called when a property bound in a
+     * <code>PropertyChangeSupport</code> this listener is added to is changed.
+     *
+     * @param theEvent A PropertyChangeEvent object describing the event source
+     *          and the property that has changed.
+     */
     @Override
     public void propertyChange(final PropertyChangeEvent theEvent) {
         if (theEvent.getPropertyName().equals(ModelProperties.LOG_LIST_MODEL_UPDATED)) {
