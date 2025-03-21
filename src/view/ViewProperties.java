87903a212d9be7fe133f2cfcd94ff1346678a8ca
Mitchell Nowasky <82383787+MitchellCodes@users.noreply.github.com>
2025-03-21 13:45:35 -0700

    Removed unused ViewProperties

--- src/view/ViewProperties.java
+++ src/view/ViewProperties.java
@@ -3,16 +3,12 @@ package view;
 public interface ViewProperties {
     String ADD_BUTTON = "startView";
     String REMOVE_BUTTON = "stopView";
-    String SUBMIT_BUTTON = "submitView";
     String CLEAR_LOG = "clearLogView";
     String CLEAR_DATABASE = "clearDatabase";
     String SAVE_LOG = "saveView";
-    String SHORTCUTS = "shortcutView";
     String ABOUT = "aboutView";
-    String START_STOP_BUTTON = "startStopView";
     String ADDED_TO_FILE_LIST_MODEL = "addedToFileListModel";
     String REMOVED_FROM_FILE_LIST_MODEL = "removedFromFileListModel";
     String DB_QUERY = "dbQueryView";
-    String TABLE_UPDATE = "tableUpdateView";
     String EMAIL = "emailView";
 }
