df0b17a24133ba53752af908c2f4708e63fe074e
Mitchell Nowasky <82383787+MitchellCodes@users.noreply.github.com>
2025-03-21 14:20:48 -0700

    Added javadoc to FileListPanel

--- src/view/FileListPanel.java
+++ src/view/FileListPanel.java
@@ -6,7 +6,6 @@ import model.ModelProperties;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -14,12 +13,37 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * An element of the GUI that shows the user a list of file extensions and directories that are being watched.
+ */
 public class FileListPanel extends JPanel implements PropertyChangeListener {
-    private static final int SMALL_COLUMN_WIDTH = 95;
-    private final JTable myJTable;
+    /**
+     * Constant int for how wide the last column in the table should be.
+     */
+    private final static int LAST_COLUMN_WIDTH = 95;
+
+    /**
+     * Constant reference to a JButton that allows the user to remove the selected extension
+     * and directory from the watcher system so changes will no longer be tracked.
+     */
     private final static JButton myRemoveButton = new JButton("Remove");
+
+    /**
+     * The JTable displayed in this panel.
+     */
+    private final JTable myJTable;
+
+    /**
+     * <code>PropertyChangeSupport</code> for notifying listeners of events.
+     */
     private final PropertyChangeSupport myPcs;
 
+    /**
+     * Creates a panel that displays a list of files and extensions that the user entered
+     * to be tracked by the watcher system. Allows the user to select rows in the table and remove them if desired.
+     *
+     * @param thePcs The <code>PropertyChangeSupport</code> that this listener should be added to.
+     */
     public FileListPanel(final PropertyChangeSupport thePcs) {
         String[][] empty2DStringArray = new String[0][0];
         myJTable = new JTable(empty2DStringArray, FileListModel.COLUMN_HEADER_ARRAY);
@@ -27,7 +51,7 @@ public class FileListPanel extends JPanel implements PropertyChangeListener {
 
         // sets width of last column to be smaller
         myJTable.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
-        myJTable.getColumnModel().getColumn(2).setMaxWidth(SMALL_COLUMN_WIDTH);
+        myJTable.getColumnModel().getColumn(2).setMaxWidth(LAST_COLUMN_WIDTH);
 
         myPcs = thePcs;
         myPcs.addPropertyChangeListener(this);
@@ -35,19 +59,22 @@ public class FileListPanel extends JPanel implements PropertyChangeListener {
         setLayout(new BorderLayout());
         JLabel fileListLabel = new JLabel("Files and directories being watched");
         add(fileListLabel, BorderLayout.NORTH);
-        initFileList();
-        initRemoveButton();
 
-        add(initRemoveButton(), BorderLayout.SOUTH);
-    }
-
-    private void initFileList() {
+        // display table
         JScrollPane listScrollPane = new JScrollPane(myJTable);
         listScrollPane.setPreferredSize(new Dimension(400, 100));
         add(listScrollPane, BorderLayout.CENTER);
+
+        initRemoveButton();
+
+        add(myRemoveButton, BorderLayout.SOUTH);
     }
 
-    private JButton initRemoveButton() {
+    /**
+     * Helper method that adds functionality to the remove button. Gets the selected row from the table
+     * sends that data to the controller so it can be removed from the model.
+     */
+    private void initRemoveButton() {
         myJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
@@ -67,9 +94,15 @@ public class FileListPanel extends JPanel implements PropertyChangeListener {
         });
         myRemoveButton.setEnabled(false);
         myRemoveButton.setMnemonic(KeyEvent.VK_T);
-        return myRemoveButton;
     }
 
+    /**
+     * This method gets called when a property bound in a
+     * <code>PropertyChangeSupport</code> this listener is added to is changed.
+     *
+     * @param theEvent A PropertyChangeEvent object describing the event source
+     *          and the property that has changed.
+     */
     @Override
     public void propertyChange(final PropertyChangeEvent theEvent) {
         if (theEvent.getPropertyName().equals(ModelProperties.FILE_LIST_MODEL_UPDATED)) {
@@ -77,7 +110,7 @@ public class FileListPanel extends JPanel implements PropertyChangeListener {
 
             // maintain the size of the columns when getting the new model
             myJTable.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
-            myJTable.getColumnModel().getColumn(2).setMaxWidth(SMALL_COLUMN_WIDTH);
+            myJTable.getColumnModel().getColumn(2).setMaxWidth(LAST_COLUMN_WIDTH);
         }
     }
 }
